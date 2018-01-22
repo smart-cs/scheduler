9d3a278f1258cd29a9dcea37a613e45de503f051
Nick <nickwu241@gmail.com>
2018-01-21 20:28:38 -0500

    Extend times to 9pm. Fixes #41

--- src/components/ScheduleComponent.vue
+++ src/components/ScheduleComponent.vue
@@ -36,7 +36,23 @@
 import CourseComponent from './CourseComponent'
 import moment from 'moment'
 
-const TIMES = ['8:00', '', '9:00', '', '10:00', '', '11:00', '', '12:00', '', '1:00', '', '2:00', '', '3:00', '', '4:00', '', '5:00', '', '06:00', '']
+const TIMES = [
+  '8:00', '',
+  '9:00', '',
+  '10:00', '',
+  '11:00', '',
+  '12:00', '',
+  '1:00', '',
+  '2:00', '',
+  '3:00', '',
+  '4:00', '',
+  '5:00', '',
+  '6:00', '',
+  '7:00', '',
+  '8:00', '',
+  '9:00', ''
+]
+
 export default {
   name: 'schedule-component',
   props: ['term', 'sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'],
