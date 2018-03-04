ee46607d5d735770ff964f5def4704fd8428d0b6
Nick <nickwu241@gmail.com>
2018-03-04 11:45:54 -0500

    Create deploy.sh to deploy to github pages

--- README.md
+++ README.md
@@ -26,18 +26,9 @@ npm test
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
 
-## Deployment Steps
-```bash
-npm run build && \
-git checkout gh-pages && \
-rm -rf static index.html && \
-mv dist/* . && \
-sed -i '' 's/\/static/static/g' index.html && \
-git add index.html static/
-
-# Add commit message
-git commit -m "Deploy"
-git push origin gh-pages
+## Deployment
+```shell
+./deploy.sh
 ```
 
 ## Implementation
---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -euo pipefail
+
+# Make sure npm and git are installed
+$(hash npm >/dev/null && hash git >/dev/null) || echo 'Make sure npm and git are in your $PATH'
+
+# Get our current branch so we can come back to it
+INITIAL_BRANCH="$(git branch | grep \* | cut -d ' ' -f2)"
+
+# Checkout to master and build
+git checkout master
+MASTER_COMMIT_SHA="$(git rev-parse --short HEAD)"
+npm run build
+
+# Checkout to gh-pages for adding newly built files
+git checkout gh-pages || git checkout -b gh-pages
+rm -rf static index.html
+mv dist/* .
+sed -i '' 's/\/static/static/g' index.html
+
+if [ "$(git diff static/ index.html)" == "" ]; then
+    echo "The deployed page is the same as the page from ${MASTER_COMMIT_SHA}"
+    git checkout ${INITIAL_BRANCH}
+    exit 1
+fi
+
+git add index.html static/
+git commit -m "Deploy page from ${MASTER_COMMIT_SHA}"
+git push origin gh-page
+
+git checkout ${INITIAL_BRANCH}
